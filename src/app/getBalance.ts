45d2b909a2305c06300e690d49aa050f5a614b16
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-10-02 18:04:35 +0200

    Convert js files to ts

--- src/app/getBalance.ts
+++ src/app/getBalance.ts
@@ -1,4 +1,4 @@
-function getBalance() {
+function getBalance(): number {
     return 17;
 }
 
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,6 @@ import { getBalance } from "./getBalance.js";
 
 console.log("App is running")
 let balance = document.getElementById("balance");
-balance.innerHTML = getBalance();
+balance!.innerHTML = getBalance().toString();
 
 
